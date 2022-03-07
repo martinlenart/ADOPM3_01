cf39348b771a4911afe118d822bbf1796d0114e5
Martin Lenart <martin_lenart@icloud.com>
2022-03-07 16:18:40 +0100

    01_14 Updated

--- ADOPM3_01_13/Program.cs
+++ ADOPM3_01_13/Program.cs
@@ -10,35 +10,39 @@ namespace ADOPM3_01_13
         static void Main(string[] args)
         {
             string[] words = "The quick brown fox jumps over the lazy dog".Split();
-            Random rand = new Random(0);
+            Random rand = new Random();
 
             StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < 1000; i++)
+            for (int i = 0; i < 1000_000; i++)
                 sb.Append(words[rand.Next(words.Length)] + " ");
             
             Console.WriteLine(sb.Length);
+            Console.WriteLine(fname("Example4_13_uncompressed.text"));    
             using (Stream s = File.Create(fname("Example4_13_uncompressed.text")))
             using (TextWriter w = new StreamWriter(s))
                 w.Write(sb);
 
             Console.WriteLine(new FileInfo(fname("Example4_13_uncompressed.text")).Length);
 
-            using (Stream s = File.Create(fname("Example4_13_compressed.bin")))
-            using (Stream ds = new DeflateStream(s, CompressionMode.Compress))
+            
+            using (Stream s = File.Create(fname("Example4_13_compressed.zip")))
+            using (Stream ds = new GZipStream(s, CompressionMode.Compress))
             using (TextWriter w = new StreamWriter(ds))
                 w.Write(sb);
 
             Console.WriteLine(new FileInfo(fname("Example4_13_compressed.bin")).Length);
 
-            using (Stream s = File.OpenRead(fname("Example4_13_compressed.bin")))
-            using (Stream ds = new DeflateStream(s, CompressionMode.Decompress))
+            
+            using (Stream s = File.OpenRead(fname("Example4_13_compressed.zip")))
+            using (Stream ds = new GZipStream(s, CompressionMode.Decompress))
             using (TextReader r = new StreamReader(ds))
                 Console.Write(r.ReadToEnd());
+            
 
             static string fname(string name)
             {
                 var documentPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
-                documentPath = Path.Combine(documentPath, "AOOP2", "Examples");
+                documentPath = Path.Combine(documentPath, "ADOP", "Examples");
                 if (!Directory.Exists(documentPath)) Directory.CreateDirectory(documentPath);
                 return Path.Combine(documentPath, name);
             }
