9a12dcc80b55c6c667ff4dbc593f0b78e6a7fc8f
Martin Lenart <martin_lenart@icloud.com>
2022-03-11 08:27:32 +0100

    update M3_01_02
    update M3_01_02
    
    update M3_01_02
    
    update M3_01_02
    
    M3_01_02

--- ADOPM3_01_02/Program.cs
+++ ADOPM3_01_02/Program.cs
@@ -6,39 +6,42 @@ namespace ADOPM3_01_02
     {
         static void Main(string[] args)
         {
-            int x = 9;
-            object obj = x;             // Box the int
-            int y = (int)obj;           // Unbox the int
+            //In is a classic value type
+            int x1 = 9;
+            int x2 = 7;
 
-            if ((int)obj == 9)
-            {
-                Console.WriteLine("Hello");
-            }
+            x1 = x2;
+            x2 = 10;
+            Console.WriteLine($"x1: {x1}, x2: {x2}");
 
-            object obj1 = 15;           // 15 is inferred to be of type int
-            long x2 = (long)(int)obj1;  // Explicit casting needed
+            //Boxing the integer - it is now a reference type
+            object ox1 = x1;
+            object ox2 = x2;
 
-            object obj2 = 15L;          // 15 is explicit to be of type long
-            long x3 = (long)obj2;       // Explicit casting 
+            ox1 = ox2;
+            ox2 = 10;
+            Console.WriteLine($"ox1: {ox1}, ox2: {ox2}");
 
-            object obj3 = 3.5;          // 3.5 is inferred to be of type double
-            int y1 = (int)(double)obj3;
+            //Unbox the ox1, ox2 (variables of reference type) back into value types
+            int y1 = (int)ox1;           
+            int y2 = (int)ox2;
 
-            //Explain
-            int i1 = 123;
-            object o1 = i1;
+            y1 = y2;
+            y2 = 15;
+            Console.WriteLine($"y1: {y1}, y2: {y2}");
 
-            int i2 = (int)o1;
-            object o2 = o1;
-            o1 = 4;
-            //Console.WriteLine(i1); // ?
-            //Console.WriteLine(i2); // ?
-            //Console.WriteLine(o1); // ?
-            //Console.WriteLine(o2); // ?
+            //Below part not part of the exercise
+            //Unbox the ox1, ox2 (variables of reference type) and cast it into a float
+            float z1 = (float)(int)ox1;
+            float z2 = (float)(int)ox2;
+
+            z1 = z2;
+            z2 = 15F;
+            Console.WriteLine($"z1: {z1}, z2: {z2}");
         }
     }
 
     //Exercise:
-    //1.    Discuss in the group and explain the printout in the Explain section
-    //2.    Create boxing and unboxing using tuples
+    //1.    Recreate the above code having x1 and x2 being of type public struct AStruct { public int iVal;}
+
 }
