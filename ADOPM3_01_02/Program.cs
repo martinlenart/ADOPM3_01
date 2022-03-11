ed6a4d3054c06ffa3e8fae6e86942df5eb5ba3f3
Martin Lenart <martin_lenart@icloud.com>
2022-03-11 12:41:23 +0100

    Boxing and Unboxing example updated

--- ADOPM3_01_02/Program.cs
+++ ADOPM3_01_02/Program.cs
@@ -6,7 +6,7 @@ namespace ADOPM3_01_02
     {
         static void Main(string[] args)
         {
-            //In is a classic value type
+            //Int is a simple, classic value type
             int x1 = 9;
             int x2 = 7;
 
@@ -14,12 +14,15 @@ namespace ADOPM3_01_02
             x2 = 10;
             Console.WriteLine($"x1: {x1}, x2: {x2}");
 
-            //Boxing the integer - it is now a reference type
+            //Boxing the integer - it is now of reference type object
             object ox1 = x1;
             object ox2 = x2;
 
             ox1 = ox2;
-            ox2 = 10;
+            Console.WriteLine($"ox1: {ox1}, ox2: {ox2}");
+
+            //Implicit unboxing makes it appear as a value type.
+            ox2 = 20;
             Console.WriteLine($"ox1: {ox1}, ox2: {ox2}");
 
             //Unbox the ox1, ox2 (variables of reference type) back into value types
@@ -30,7 +33,6 @@ namespace ADOPM3_01_02
             y2 = 15;
             Console.WriteLine($"y1: {y1}, y2: {y2}");
 
-            //Below part not part of the exercise
             //Unbox the ox1, ox2 (variables of reference type) and cast it into a float
             float z1 = (float)(int)ox1;
             float z2 = (float)(int)ox2;
@@ -40,8 +42,4 @@ namespace ADOPM3_01_02
             Console.WriteLine($"z1: {z1}, z2: {z2}");
         }
     }
-
-    //Exercise:
-    //1.    Recreate the above code having x1 and x2 being of type public struct AStruct { public int iVal;}
-
 }
--- ADOPM3_01_02a/Program.cs
+++ ADOPM3_01_02a/Program.cs
@@ -7,47 +7,36 @@ namespace ADOPM3_01_02a
         public struct AStruct { public int iVal; }
         static void Main(string[] args)
         {
-            //In is a classic value type
+            //AStruct is a complex value type
             AStruct x1 = new AStruct { iVal = 9 };
             AStruct x2 = new AStruct { iVal = 7 };
 
             x1 = x2;
-            x2 = new AStruct { iVal = 10 };
+            x2.iVal = 10;
             Console.WriteLine($"x1: {x1.iVal}, x2: {x2.iVal}");
 
-
-            //Boxing the integer - it is now a reference type
+            //Boxing the AStruct - it is now of reference type object
             object ox1 = x1;
             object ox2 = x2;
 
-            ox2 = new AStruct { iVal = 20 };
             ox1 = ox2;
+            Console.WriteLine($"ox1: {((AStruct)(ox1)).iVal }, ox2: {((AStruct)(ox2)).iVal}");
 
-            Console.WriteLine($"ox1: {((AStruct)ox1).iVal}, ox2: {((AStruct)ox2).iVal}");
+            //Implicit unboxing makes it appear as a value type.
+            //==> that is why I cannot change content ox2
+            //((AStruct)(ox2)).iVal = 20; //compiler error
 
+            //==> but I can create a new instance of ox2
+            ox2 = new AStruct { iVal = 20 };
+            Console.WriteLine($"ox1: {((AStruct)(ox1)).iVal }, ox2: {((AStruct)(ox2)).iVal}");
 
             //Unbox the ox1, ox2 (variables of reference type) back into value types
             AStruct y1 = (AStruct)ox1;
             AStruct y2 = (AStruct)ox2;
 
             y1 = y2;
-            y2 = new AStruct { iVal = 15 };
+            y2.iVal = 15;
             Console.WriteLine($"y1: {y1.iVal}, y2: {y2.iVal}");
-
-            /*
-            //Below part not part of the exercise
-            //Unbox the ox1, ox2 (variables of reference type) and cast it into a float
-            float z1 = (float)(int)ox1;
-            float z2 = (float)(int)ox2;
-
-            z1 = z2;
-            z2 = 15F;
-            Console.WriteLine($"z1: {z1}, z2: {z2}");
-            */
         }
     }
-
-    //Exercise:
-    //1.    Recreate the above code having x1 and x2 being of type public struct AStruct { public int iVal;}
-
 }
